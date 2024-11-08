ac55511b4191678a1a0270eb9517e6c35b7a2a7f
rsx <contato.rodrigosilv@gmail.com>
2024-11-08 16:13:13 -0300

    :sparkles: feat: setup mongodb in application

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import fastify from "fastify";
+import fastifyMongo from "@fastify/mongodb";
 import { router } from "./router.ts";
+import fastify from "fastify";
 
 export const app = fastify();
 
 app.register(router);
+
+app.register(fastifyMongo, {
+  url: "mongodb://localhost:27017/called"
+});
 
 app.listen({ port: 8080 }, (err, address) => {
   if (err) console.log(err);
